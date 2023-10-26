37c2c966f0642d7f7f63c966e4f65bb98e9f1c2c
Şeref Kaynak <serefkynk@gmail.com>
2023-10-26 15:21:39 +0300

    Material UI used and integrated code.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, {useState, useEffect }from 'react';
+import axios from 'axios';
+import Karakter from './components/Karakter';
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -6,30 +10,34 @@ const App = () => {
 
   const [characters, setCharacters] = useState([]);
 
-
   useEffect(() => {
-    fetch('https://swapi.dev/api/people/')
-      .then(response => response.json())
-      .then(data => setCharacters(data.results));
+    axios.get('https://swapi.dev/api/people/')
+    .then(res => {
+      setCharacters(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
   },[]);
-
-
-  console.log(characters);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-    return (
-      <div className="App">
-        <h1 className="Header">Karakterler</h1>
-        <ul>            
-          {characters.map((character, index) => (
-            <li key={index}>{character.name}</li>
-          ))}
-        </ul>
+  return (
+    <div className="App">
+      <h1 className="Header">Star Wars Characters</h1>
+      <div className='accordion-div'>
+        {characters.map(kisi => {
+          return <div 
+                    className='accordion'>
+                      <Karakter kisi={kisi} />
+                  </div>
+        })}
       </div>
-    );
+
+    </div>
+  );
 }
 
 export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,3 +1,48 @@
 // Karakter bileşeniniz buraya gelecek
-import React from "react";
-import { useState, useEffect  } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+
+export default function Karakter(props) {
+
+    console.log(props.kisi, ' PROPS')
+
+    const [gezegen, setGezegen] = useState(''); 
+
+    const kisi = props.kisi
+
+    useEffect(() => {
+        axios.get(kisi.homeworld)
+            .then(function (res) {
+                setGezegen(res.data.name)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }, [])
+
+    return (
+       <Accordion className='accordion-item'>
+        <AccordionSummary
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{ kisi.name }</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <p>Gender: <strong>{kisi.gender}</strong></p>
+            <p>Hair color: <strong>{kisi.hair_color}</strong> </p>
+            <p>Height: <strong>{kisi.height}</strong></p>
+            <p>Homeworld: <strong>{ gezegen }</strong></p>
+            <p>Mass:<strong>{kisi.mass}</strong></p>
+            <p>Skin color: <strong>{kisi.skin_color}</strong></p>
+            <p>Movies:<strong>{ kisi.films.join(', ') }</strong></p>
+         </Typography>
+        </AccordionDetails>
+      </Accordion>
+    )
+}
